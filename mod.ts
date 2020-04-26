a32d7e4152c9646cc965dde8c5574bfbb0a53192
Rasmus Vermeulen <vermeulen.rasmus@gmail.com>
2020-04-26 13:52:33 +0200

    Update mod.ts/it signature

--- mod.ts
+++ mod.ts
@@ -2,7 +2,7 @@ import * as m from "./mock.ts";
 export const mock = m;
 
 export * from "./expect.ts";
-export function it(name, fn) {
+export function it(name: string, fn: () => void | Promise<void>) {
   Deno.test({
     name,
     fn,
